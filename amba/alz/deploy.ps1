b6622602967d571455d524cef37aa12176d9a6a6
tulpy <stephen.tulp@insight.com>
2024-12-18 10:18:16 +0800

    Update deployment scripts and parameters for AMBA configuration in Azure

--- amba/alz/deploy.ps1
+++ amba/alz/deploy.ps1
@@ -1,6 +1,6 @@
 New-AzManagementGroupDeployment `
     -ManagementGroupId 'mg-alz1' `
     -Location 'australiaeast' `
-    -TemplateParameterFile 'src/configuration/lz.bicepparam' `
-    -TemplateFile 'src/orchestration/main.bicep' `
+    -TemplateParameterFile './alzArm.param.json' `
+    -TemplateUri 'https://raw.githubusercontent.com/Azure/azure-monitor-baseline-alerts/main/patterns/alz/alzArm.json' `
     -Verbose
